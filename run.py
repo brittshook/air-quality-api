22456c7c10b1f6a445e022b50c2d95ae659380a7
Britt Shook <brittanysshook@gmail.com>
2024-01-21 23:27:19 -0500

    add procfile for deployment

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app.api import app
 
-if __name__ == '__main__':
-    app = app.create_app()
-    app.run(debug=False)
+app = app.create_app()
 
+if __name__ == '__main__':
+    app.run(debug=False)
