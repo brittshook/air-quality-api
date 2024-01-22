81b7089452e996e8b9c11b6e0fb3f8464786d8c2
Britt Shook <brittanysshook@gmail.com>
2024-01-21 21:21:22 -0500

    404 error handling, add webhook, cleanup resources

--- app/api/app.py
+++ app/api/app.py
@@ -1,51 +1,61 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
-from settings import config
-from models import system_status as system_status_db, SystemStatus
-from models import air_quality_data as air_quality_data_db, AirQualityData
-from models import subscriptions as subscriptions_db, Subscriptions
-from resources.alerts.subscription import Subscription
-from resources.alerts.thresholds import Thresholds
-from resources.air_quality.current import Current
-from resources.air_quality.history import History
-from resources.air_quality.pm2_5 import PM2_5
-from resources.air_quality.pm10 import PM10
-from resources.air_quality.aqi import AQI
-from resources.status import Status
+# from settings import config
+from .models.db import db
+from .resources.alerts.subscription import CreateSubscription, ManageSubscription
+from .resources.alerts.thresholds import Thresholds
+from .resources.air_quality.current import Current
+from .resources.air_quality.history import History
+from .resources.air_quality.pm2_5 import PM2_5
+from .resources.air_quality.pm10 import PM10
+from .resources.air_quality.aqi import AQI
+from .resources.status import Status
+from .webhooks import webhooks_bp
 
 def create_app():
     app = Flask(__name__)
-
+    
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{config["db_user"]}:{config["db_password"]}@{config["db_host"]}:{config["db_port"]}/{config["db_database"]}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
-    system_status_db.init_app(app)
-    air_quality_data_db.init_app(app)
-    subscriptions_db.init_app(app)
-
+    db.init_app(app)
+    
     with app.app_context():
-        system_status_db.create_all()
-        air_quality_data_db.create_all()
-        subscriptions_db.create_all()
+        db.create_all()
         
+    @app.before_request
+    def handle_not_found():
+        request.not_found_error = False
+            
     @app.errorhandler(ValueError)
     def handle_value_error(error):
-        response = jsonify({"error": str(error)})
-        response.status_code = 400
-        return response
+        return jsonify({"error": str(error)}), 400
+    
+    @app.errorhandler(404)
+    def handle_not_found_error(e):
+        request.not_found_error = True
+        return jsonify({'error': 'Resource not found'}), 404
 
     api = Api(app)
     register_resources(api)
+    app.register_blueprint(webhooks_bp)
 
     return app
 
 def register_resources(api):
-    api.add_resource(Subscription, '/api/v1/alerts/subscriptions', '/<string:id>')
-    api.add_resource(Thresholds, '/api/v1/alerts/thresholds', '/<string:id>')
-    api.add_resource(Current, '/api/v1/air_quality')
-    api.add_resource(History, '/api/v1/air_quality/history')
-    api.add_resource(PM2_5, '/api/v1/air_quality/pm2_5')
-    api.add_resource(PM10, '/api/v1/air_quality/pm10')
-    api.add_resource(AQI, '/api/v1/air_quality/aqi')
-    api.add_resource(Status, '/api/v1/status')
+    api_base_uri = '/api'
+    version = '/v1'
+    
+    api.add_resource(CreateSubscription, f'{api_base_uri}{version}/alerts/subscriptions')
+    api.add_resource(ManageSubscription, f'{api_base_uri}{version}//alerts/subscriptions/<string:id>')
+    api.add_resource(Thresholds, f'{api_base_uri}{version}/alerts/thresholds/<string:id>')
+    api.add_resource(Current, f'{api_base_uri}{version}/air_quality')
+    api.add_resource(History, f'{api_base_uri}{version}/air_quality/history')
+    api.add_resource(PM2_5, f'{api_base_uri}{version}/air_quality/pm2_5')
+    api.add_resource(PM10, f'{api_base_uri}{version}/air_quality/pm10')
+    api.add_resource(AQI, f'{api_base_uri}{version}/air_quality/aqi')
+    api.add_resource(Status, f'{api_base_uri}{version}/status')
+   
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=False)
