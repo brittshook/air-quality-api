c13325b7a649137e54d6c29d09721d03dfe63e06
Britt Shook <brittanysshook@gmail.com>
2024-01-22 00:23:01 -0500

    update settings config

--- settings.py
+++ settings.py
@@ -1,14 +1,11 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
 config = {
-    'db_host': os.getenv('DB_HOST'),
-    'db_database': os.getenv('DB_DATABASE'),
-    'db_user': os.getenv('DB_USER'),
-    'db_password': os.getenv('DB_PASSWORD'),
-    'db_port': os.getenv('DB_PORT'),
-    'brevo_api_key': os.getenv('BREVO_API_KEY'),
-    'admin_api_key': os.getenv('ADMIN_API_KEY'),
+    'db_host': os.environ.get('DB_HOST'),
+    'db_database': os.environ.get('DB_DATABASE'),
+    'db_user': os.environ.get('DB_USER'),
+    'db_password': os.environ.get('DB_PASSWORD'),
+    'db_port': os.environ.get('DB_PORT'),
+    'brevo_api_key': os.environ.get('BREVO_API_KEY'),
+    'admin_api_key': os.environ.get('ADMIN_API_KEY'),
 }
