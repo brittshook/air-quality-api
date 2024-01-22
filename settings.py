93edf6a3f356b3fb2dd5ad2a6298564636fe10c5
Britt Shook <brittanysshook@gmail.com>
2024-01-22 01:00:20 -0500

    update db url

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 config = {
-    'database_url': os.environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://'),
+    'database_url': os.environ.get('DB_URL'),
     'brevo_api_key': os.environ.get('BREVO_API_KEY'),
     'admin_api_key': os.environ.get('ADMIN_API_KEY'),
 }
