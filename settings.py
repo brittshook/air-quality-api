6e7fe06081132ad53f1b76fdc3f1082ab4ba5c4d
Britt Shook <brittanysshook@gmail.com>
2024-01-23 13:26:54 -0500

    update db config

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 config = {
-    'database_url': os.environ.get('DB_URL'),
+    'database_url': os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql+psycopg2://', 1),
     'brevo_api_key': os.environ.get('BREVO_API_KEY'),
     'admin_api_key': os.environ.get('ADMIN_API_KEY'),
 }
